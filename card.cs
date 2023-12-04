dca299a01319cf414d70f63327d5fe70f7b1fda3
John1c <nichlaskaasleth@gmail.com>
2023-12-04 13:13:19 +0100

    yippe

---
+++ card.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class card(int cardnumber, int cardsuit) : Node2D
+{
+	this.number = cardnumber;
+	this.suit = cardsuit;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
