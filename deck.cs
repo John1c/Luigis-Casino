d39b85173d0fdcfb2c2d80fcea8aa02286f7fd22
John1c <nichlaskaasleth@gmail.com>
2023-12-30 14:59:59 +0100

    Tilføjer dannelse af kort skelet

--- deck.cs
+++ deck.cs
@@ -6,10 +6,18 @@ public partial class deck : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-}
+};
+	for(int i = 1; i < 4; i++);
+	{
+		for (int j = 1; j < 13; j++)
+		{
+			new card(j,i);
+		}
+	};
