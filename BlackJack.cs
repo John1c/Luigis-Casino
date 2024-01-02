0f51ea8f3405bfe93810f607043f0b56922ccdd3
mondqay <46741103+Nordes123@users.noreply.github.com>
2024-01-02 23:34:12 +0100

    Update BlackJack.cs

--- BlackJack.cs
+++ BlackJack.cs
@@ -1,9 +1,13 @@
+using CardSystem;
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class BlackJack : Control
 {
-
+	playerHand player;
+	playerHand dealer;
+	Deck deck;
 	[Export] public TextureButton HitButton;
 
 	[Export] public TextureButton StandButton;
@@ -14,7 +18,13 @@ public partial class BlackJack : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-				
+		
+		player = new playerHand(false);
+		dealer = new playerHand(true);
+		deck = new Deck();
+		deck.createDeck();
+		deck.shuffleDeck();
+
 	}
 
 
@@ -22,12 +32,24 @@ public partial class BlackJack : Control
 	public void _on_HitButton_up()
 	{
 		GD.Print("Hit");
+
+		//give another card to player
+		player.cards.Add(deck.drawRandomCard());
+		//check if player is bust
+		if(player.handValue > 21)
+		{
+			GD.Print("Bust");
+			//end game
+		}
+		//if bust, end game
 	}
 
 
 	public void _on_StandButton_up()
 	{
 		GD.Print("Stand");
+		//end player turn
+		//start dealer turn
 	}
 	public void _on_MenuButton_up()
 	{
@@ -39,5 +61,21 @@ public partial class BlackJack : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	
+	public static string getWinState(playerHand player, playerHand dealer)
+	{
+		if	   (player.handValue > 21)
+			return "lost";
+		else if(dealer.handValue > 21)
+			return "won";
+		else if(dealer.handValue == 21 && player.handValue == 21 && dealer.cards.Count == 2 && player.cards.Count == 2)
+			return "tie";
+		else if(dealer.handValue >= player.handValue)
+			return "lost";
+		else if (dealer.handValue < player.handValue)
+			return "won";
+		else
+			return "error could not determine win state";
 	}
 }
