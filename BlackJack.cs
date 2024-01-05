14604557162dd34776c8f51ef8b34b66b62dbf1c
Zebratic <zebraticyt@gmail.com>
2024-01-05 01:23:22 +0100

    Finished
    Bug note:
    if dealer has an ace that reverts to a 1, and total matches a push condition with the player, it will be push, the dealer should hit as it was 14

--- BlackJack.cs
+++ BlackJack.cs
@@ -2,6 +2,7 @@ using CardSystem;
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
@@ -23,39 +24,74 @@ public partial class BlackJack : Control
 	public playerHand player;
 	public playerHand dealer;
 	public Deck deck;
+
+	public bool isRoundStarted = false;
 	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// if player is null, clear cards objects
+		if (player != null) player.clearCards();
+		if (dealer != null) dealer.clearCards();
+
 		player = new playerHand(false);
 		dealer = new playerHand(true);
 		deck = new Deck(); // this creates a new deck of cards and shuffles them
+		isRoundStarted = false;
+
 		// update player balance
 		setBalance(30);
-		// update renderer
+
+		// set bet
+		BetButton.Show();
 		player.betAmount = 0;
+
 		// hide win/lose screen
 		WinScreen.Hide();
 		LoseScreen.Hide();
 		winStateLabel.Text = "";
-		// hide round start button
+
+		// hide round start, stand and hit button
 		RoundStartButton.Hide();
-		RoundStart();
+		StandButton.Hide();
+		HitButton.Hide();
 	}
 
-	public TextureButton _on_RoundStartButton_up()
-	{	
+	public void _on_RoundStartButton_up()
+	{
+		// hide win/lose screen
+		WinScreen.Hide();
+		LoseScreen.Hide();
+		winStateLabel.Text = "";
+
+		// hide round start, stand and hit button
 		RoundStartButton.Hide();
+		StandButton.Hide();
+		HitButton.Hide();
+
+		// show bet button
+		BetButton.Show();
+
+		// reset bet amount
 		player.betAmount = 0;
-		GD.Print("Round Start");
-		RoundStart();	
+
+		// reset round started
+		isRoundStarted = false;
+
+		// show total bet pool
 		pooltexture.Show();
-		return null;
 	}
 
 	public void RoundStart()
 	{
+		GD.Print("Starting game with " + player.betAmount + " bet");
+		BetButton.Hide();
+		RoundStartButton.Hide();
+		HitButton.Show();
+		StandButton.Show();
+		winStateLabel.Text = "";
+
 		// reset player and dealer hands
 		player.clearCards();
 		dealer.clearCards();
@@ -68,22 +104,11 @@ public partial class BlackJack : Control
 		dealer.cards.Add(deck.drawRandomCard());
 		dealer.cards.Add(deck.drawRandomCard(true)); // hide the second card
 
-		GD.Print("Updating renderer...");
 		UpdateRenderer();
 
-		//check if player has blackjack
-		if(player.handValue == 21)
-		{
+		//check if player has blackjack right away
+		if (player.handValue == 21)
 			_on_StandButton_up();
-		}
-		
-		winStateLabel.Text = "";
-		
-		if(BetButton.Disabled == true)
-		{
-			BetButton.Disabled = false;
-			BetButton.Show();
-		}
 	}
 
 	public void UpdateRenderer()
@@ -101,46 +126,41 @@ public partial class BlackJack : Control
 		int margin = 10;
 		int yOffset = 50;
 
+		// render player cards
 		float playerCardsWidth = player.cardObjects.Count * (cardSize.X + margin) - margin;
 		float playerCardsX = (gameWindow.Size.X - playerCardsWidth) / 2;
 		float playerCardsY = gameWindow.Size.Y / 4 * 3 - cardSize.Y / 2 + yOffset;
-
 		for (int i = 0; i < player.cardObjects.Count; i++)
-		{
 			player.cardObjects[player.cards[i]].Position = new Vector2(playerCardsX + i * (cardSize.X + margin), playerCardsY);
-		}
 
-		// dealer cards
+		// render dealer cards
 		float dealerCardsWidth = dealer.cardObjects.Count * (cardSize.X + margin) - margin;
 		float dealerCardsX = (gameWindow.Size.X - dealerCardsWidth) / 2;
 		float dealerCardsY = gameWindow.Size.Y / 4 - cardSize.Y / 2 + yOffset;
-
 		for (int i = 0; i < dealer.cardObjects.Count; i++)
-		{
 			dealer.cardObjects[dealer.cards[i]].Position = new Vector2(dealerCardsX + i * (cardSize.X + margin), dealerCardsY);
-		}
 
 		// print hand value of both player and dealer (print dealer first like 6+4 = 10)
 		GD.Print("Dealer hand: ");
 		for (int i = 0; i < dealer.cards.Count; i++)
-		{
 			GD.Print("+" + dealer.cards[i].value);
-		}
-		GD.Print("Dealer hand value: " + dealer.handValue);
+		GD.Print("=" + dealer.handValue);
 
 		// print player hand value
 		GD.Print("Player hand: ");
 		for (int i = 0; i < player.cards.Count; i++)
-		{
 			GD.Print("+" + player.cards[i].value);
-		}
-		GD.Print("Player hand value: " + player.handValue);
+		GD.Print("=" + player.handValue);
 	}
 
 
 
 	public void _on_BetButton_up()
 	{
+		// if first bet of round, show hit button
+		if (!isRoundStarted && player.betAmount == 0)
+			HitButton.Show();
+
 		if (player.Balance >= 5)
 		{
 			player.betAmount += 5;
@@ -154,17 +174,23 @@ public partial class BlackJack : Control
 
 		// if balance is 0, disable bet button
 		if (player.Balance == 0)
-		{
-			BetButton.Disabled = true;
 			BetButton.Hide();
-			
-		}
-		UpdateRenderer();
-		GD.Print(player.betAmount);
+
+		updateBalance();
+		GD.Print("Bet amount upped to: " + player.betAmount);
 	}
 
 	public void _on_HitButton_up()
 	{
+		// if round has not started, start round
+		if (!isRoundStarted)
+		{
+			RoundStart();
+			isRoundStarted = true;
+			return;
+		}
+
+
 		GD.Print("Hit");
 		Card newcard = deck.drawRandomCard();
 
@@ -201,79 +227,57 @@ public partial class BlackJack : Control
 	public void _on_StandButton_up()
 	{
 		GD.Print("Stand");
-		//reveal dealer card
-		dealer.cards[1].isHidden = false;
-		UpdateRenderer();
+		dealer.cards[1].isHidden = false; //reveal the hidden dealer card
+
+		// hide stand & hit button
+		StandButton.Hide();
+		HitButton.Hide();
+
 		while(getWinState() == WinState.Continue)
 		{
 			dealer.cards.Add(deck.drawRandomCard());
-			UpdateRenderer();
 		}
-		// get win state
-		GD.Print("dealer hand value:" + dealer.handValue);
+
 		switch(getWinState())
 		{
 			case WinState.Lost:
 				GD.Print("You lost");
-				UpdateRenderer();
 				winStateLabel.Text = "You lost";
-				if(player.Balance <= 0)
-				{
+				if (player.Balance <= 0) // if player has no money left, show lose screen
 					LoseScreen.Show();
+				else { // else show round start button
+					RoundStartButton.Show();
+					pooltexture.Hide();
 				}
-				
-				//restart game
-				RoundStartButton.Show();
-				pooltexture.Hide();
 				break;
 			case WinState.Won:
 				GD.Print("You won");
-				
 				winStateLabel.Text = "You won";
-				//points to player
 				player.Balance += player.betAmount * 2;
-				UpdateRenderer();
-				//restart game
 				RoundStartButton.Show();
 				pooltexture.Hide();
 				break;
 			case WinState.Push:
-				
 				GD.Print("Push");
-				
 				winStateLabel.Text = "Push";
-				// money back
-				player.Balance += player.betAmount;
-				UpdateRenderer();
-				//restart game
+				player.Balance += player.betAmount; // money back
 				RoundStartButton.Show();
 				pooltexture.Hide();
 				break;
 			case WinState.BlackJack:
 				GD.Print("BlackJack");
-				
 				winStateLabel.Text = "Blackjack";
-				// money back + 2.0x
-				player.Balance += player.betAmount * 3;
-				UpdateRenderer();
-				//restart game
+				player.Balance += player.betAmount * 3; // win 3x bet
 				RoundStartButton.Show();
 				pooltexture.Hide();
-
 				break;
-			case WinState.Unknown:
-				GD.Print("Error: getWinState() returned unknown");
-				break;
-			case WinState.Continue:
-				GD.Print("Error: getWinState() returned continue");
-				break;
-			default:
-				GD.Print("Super Error: getWinState() returned default case... how tf did you manage to fo this");
-				break;
+			case WinState.Unknown: GD.Print("Error: getWinState() returned unknown"); break;
+			case WinState.Continue: GD.Print("Error: getWinState() returned continue"); break;
+			default: GD.Print("Super Error: getWinState() returned default case..."); break;
 		}
-
-		
+		UpdateRenderer(); // update renderer to show dealer cards
 	}
+	
 	public void _on_MenuButton_up()
 	{
 		GD.Print("Menu");
@@ -305,7 +309,7 @@ public partial class BlackJack : Control
 		else
 		{
 			GD.Print("Error: getWinState() returned default case");
-			return WinState.Unknown; // push on default case
+			return WinState.Push; // push on default case
 		}
 	}
 
